a40773c7182be64c26ce891bb090f3fa7ed4d525
nishan <bhattarainisham@gmail.com>
2025-11-24 16:35:39 +0545

    change GET routes to POST for registration and login; add .gitignore file

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -3,11 +3,11 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/register',  async (req, res) => {
+router.post('/register',  async (req, res) => {
     // Handle registration
     res.send("Register");
 });
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     // Handle login
     res.send("Login");
 });
