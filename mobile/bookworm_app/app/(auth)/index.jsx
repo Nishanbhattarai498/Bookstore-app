622cdc9f8b2b27eaeb7ebd158467aceba4b20568
nishan <bhattarainisham@gmail.com>
2025-11-27 15:58:47 +0545

    feat: enhance login screen with input fields and layout adjustments

--- mobile/bookworm_app/app/(auth)/index.jsx
+++ mobile/bookworm_app/app/(auth)/index.jsx
@@ -1,9 +1,81 @@
-import { View, Text } from 'react-native'
+import { View, Text,Image } from 'react-native'
+import styles from '@/assets/styles/login.styles'
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+import COLORS from '@/constants/colors';
+
 
 export default function Login() {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState(false);
+
+    const handleLogin = () => {
+        // Handle login logic here
+    }
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.container}> 
+     {/* ILLUSTRATION */}
+        <View style={styles.topIllustration}>
+          <Image
+            source={require("../../assets/images/i.png")}
+            style={styles.illustrationImage}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.card}>
+
+        <View style={styles.formContainer}>
+             {/* EMAIL */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <View style={styles.inputContainer}>
+                <Ionicons
+                  name="mail-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your email"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+              </View>
+            </View>
+
+            {/* PASSWORD */}
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <View style={styles.inputContainer}>
+                {/* LEFT ICON */}
+                <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                />
+                {/* INPUT */}
+                <TextInput
+                  style={styles.input}
+                  placeholder="Enter your password"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+
+        </View>
+        </View>
+       
+      </View>
+      </View>
     </View>
   )
 }
--- mobile/bookworm_app/app/_layout.jsx
+++ mobile/bookworm_app/app/_layout.jsx
@@ -2,14 +2,17 @@ import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "@/components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <SafeScreen>
-        <Stack  screenOptions={{headerShown:false}}/>
-      </SafeScreen>
-      <StatusBar style="dark" />
-    </SafeAreaProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <SafeScreen>
+          <Stack  screenOptions={{headerShown:false}}/>
+        </SafeScreen>
+        <StatusBar style="dark" />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
