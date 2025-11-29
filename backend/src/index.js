34c37329093d0710c2ea6930844d93841184369f
nishan <bhattarainisham@gmail.com>
2025-11-29 10:49:57 +0545

    feat: add cron job to send GET request every 14 minutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,15 @@ import bookRoutes from './routes/bookRoutes.js';
 import cors from 'cors';
 import { connect } from 'mongoose';
 import { connectDB } from './lib/db.js';
+import jobs from './lib/jobs.js';
 
 dotenv.config();
 
 
 const app = express();
 const PORT  = process.env.PORT;
+
+jobs.start();
 
 app.use(express.json());
 app.use(cors());
